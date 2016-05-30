72d3973d6f1017a0284f21ada6361b28fe960545
Iris Caffin <iriscaffin@gmail.com>
2016-05-29 21:32:49 -0700

    Gives error to change value

---
+++ ConstantVsReadOnly/ConstantVsReadOnly/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstantVsReadOnly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test obj = new Test();
+            obj.Check();
+            Console.ReadLine();           
+        }
+    }
+    class Test
+    {
+        readonly int read = 10;
+        const int cons = 10;
+        public Test()
+        {
+            read = 100;
+            cons = 100;
+        }
+        public void Check()
+        {
+            Console.WriteLine("Read only : {0}", read);
+            Console.WriteLine("const : {0}", cons);
+        }
+    }
+}
